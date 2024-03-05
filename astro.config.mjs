e6756fdd740a9f6729b7f98845945604c9f8412d
James Brattin <james.brattin@gmail.com>
2024-03-05 23:28:10 +0100

    added preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jamesbrattin.dev"
+  site: "https://jamesbrattin.dev",
+  integrations: [preact()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'James Brattin Blog',
+    title: "James Brattin's blog",
     description: 'The personal blog of James Brattin. I write about web development, programming, and sometimes other things.',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
