5cefcd0de499e5bad51e24330df398c80a9df1dc
James Brattin <james.brattin@gmail.com>
2024-03-05 21:53:21 +0100

    added rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'James Brattin Blog',
+    description: 'The personal blog of James Brattin. I write about web development, programming, and sometimes other things.',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
